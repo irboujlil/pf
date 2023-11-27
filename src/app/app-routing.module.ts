db534b525821e30618d7b81d9ed86456e26db325
ilias boujlil <iliasboujlil@iliass-MBP.lan>
2023-11-27 10:32:22 -0600

    Adding more files 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat-component/chat-component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  // Define other routes here
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
