63d68c2930a410e08537b1dd122bd4d2d760b4d7
ilias boujlil <iliasboujlil@iliass-MBP.lan>
2023-11-27 11:16:42 -0600

    Adding more files 7

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -9,22 +9,22 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get('/api/data'); // Adjust the endpoint as needed
+    return this.http.get('https://pocket-pf-backend-988d2f637df6.herokuapp.com/api/data'); // Adjust the endpoint as needed
   }
 
   sendMessage(message: any) {
     //console.log(message);
-    return this.http.post('/api/assist', {"prompt": message});
+    return this.http.post('https://pocket-pf-backend-988d2f637df6.herokuapp.com/api/assist', {"prompt": message});
   }
 
   sendPDF(file: any) {
     //console.log(file);
-    return this.http.post('/api/upload-pdf', {"file": file});
+    return this.http.post('https://pocket-pf-backend-988d2f637df6.herokuapp.com/api/upload-pdf', {"file": file});
   }
 
   sendFile(base64: string, filename: string) {
     //console.log(file);
     const body = { filename: base64, filedata: filename };
-    return this.http.post('/api/image', body);
+    return this.http.post('https://pocket-pf-backend-988d2f637df6.herokuapp.com/api/image', body);
   }
 }
